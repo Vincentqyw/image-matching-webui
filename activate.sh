dd2401c635088310e628519076b978f05c97fd36
Realcat <realcat@126.com>
2024-07-07 08:35:50 +0000

    del: activate.sh

--- activate.sh
+++
@@ -1,6 +0,0 @@
-# download submodules
-git submodule update --init --recursive
-
-# configuration envs
-pip install -r env-docker.txt
-sudo apt-get update && sudo apt-get install ffmpeg libsm6 libxext6 -y
