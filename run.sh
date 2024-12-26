5c18923f295445ef5c99468b7d7fbcc2e41aa555
AGI <33508327+agipro@users.noreply.github.com>
2024-12-26 16:15:00 +0000

    update: ci

---
+++ run.sh
@@ -0,0 +1,81 @@
+# git rm --cached path/to/submodule
+
+# git submodule deinit -f -- third_party/SOLD2
+# git submodule deinit -f -- third_party/GlueStick
+# git submodule deinit -f -- third_party/ASpanFormer
+# git submodule deinit -f -- third_party/TopicFM
+# git submodule deinit -f -- third_party/d2net
+# git submodule deinit -f -- third_party/r2d2
+# git submodule deinit -f -- third_party/DKM
+# git submodule deinit -f -- third_party/ALIKE
+# git submodule deinit -f -- third_party/LightGlue
+# git submodule deinit -f -- third_party/DeDoDe
+# git submodule deinit -f -- third_party/RoRD
+# git submodule deinit -f -- third_party/lanet
+# git submodule deinit -f -- third_party/mickey
+# git submodule deinit -f -- third_party/SuperGluePretrainedNetwork
+# git submodule deinit -f -- third_party/COTR
+# git submodule deinit -f -- third_party/gim
+# git submodule deinit -f -- third_party/dust3r
+# git submodule deinit -f -- third_party/omniglue
+# git submodule deinit -f -- third_party/RoMa
+# git submodule deinit -f -- third_party/SGMNet
+# git submodule deinit -f -- third_party/DarkFeat
+# git submodule deinit -f -- third_party/mast3r
+# git submodule deinit -f -- third_party/pram
+# git submodule deinit -f -- third_party/EfficientLoFTR
+
+
+# git rm --cached  third_party/SOLD2
+# git rm --cached  third_party/GlueStick
+# git rm --cached  third_party/ASpanFormer
+# git rm --cached  third_party/TopicFM
+# git rm --cached  third_party/d2net
+# git rm --cached  third_party/r2d2
+# git rm --cached  third_party/DKM
+# git rm --cached  third_party/ALIKE
+# git rm --cached  third_party/LightGlue
+# git rm --cached  third_party/DeDoDe
+# git rm --cached  third_party/RoRD
+# git rm --cached  third_party/lanet
+# git rm --cached  third_party/mickey
+# git rm --cached  third_party/SuperGluePretrainedNetwork
+# git rm --cached  third_party/COTR
+# git rm --cached  third_party/gim
+# git rm --cached  third_party/dust3r
+# git rm --cached  third_party/omniglue
+# git rm --cached  third_party/RoMa
+# git rm --cached  third_party/SGMNet
+# git rm --cached  third_party/DarkFeat
+# git rm --cached  third_party/mast3r
+# git rm --cached  third_party/pram
+# git rm --cached  third_party/EfficientLoFTR
+# git rm --cached  third_party/XoFTR
+
+
+
+git submodule add https://github.com/cvg/SOLD2.git imcui/third_party/SOLD2
+git submodule add https://github.com/cvg/GlueStick.git imcui/third_party/GlueStick
+git submodule add https://github.com/Vincentqyw/ml-aspanformer.git imcui/third_party/ASpanFormer
+git submodule add https://github.com/Vincentqyw/TopicFM.git imcui/third_party/TopicFM
+git submodule add https://github.com/Vincentqyw/d2-net.git imcui/third_party/d2net
+git submodule add https://github.com/naver/r2d2.git imcui/third_party/r2d2
+git submodule add https://github.com/Vincentqyw/DKM.git imcui/third_party/DKM
+git submodule add https://github.com/Shiaoming/ALIKE.git imcui/third_party/ALIKE
+git submodule add https://github.com/cvg/LightGlue.git imcui/third_party/LightGlue
+git submodule add https://github.com/Parskatt/DeDoDe.git imcui/third_party/DeDoDe
+git submodule add https://github.com/Vincentqyw/RoRD.git imcui/third_party/RoRD
+git submodule add https://github.com/agipro/lanet.git imcui/third_party/lanet
+git submodule add https://github.com/agipro/mickey.git imcui/third_party/mickey
+git submodule add https://github.com/Vincentqyw/SuperGluePretrainedNetwork.git imcui/third_party/SuperGluePretrainedNetwork
+git submodule add https://github.com/Vincentqyw/COTR.git imcui/third_party/COTR
+git submodule add https://github.com/xuelunshen/gim.git imcui/third_party/gim
+git submodule add https://github.com/naver/dust3r.git imcui/third_party/dust3r
+git submodule add https://github.com/Vincentqyw/omniglue-onnx.git imcui/third_party/omniglue
+git submodule add https://github.com/Vincentqyw/RoMa.git imcui/third_party/RoMa
+git submodule add https://github.com/agipro/SGMNet.git imcui/third_party/SGMNet
+git submodule add https://github.com/agipro/DarkFeat.git imcui/third_party/DarkFeat
+git submodule add https://github.com/naver/mast3r.git imcui/third_party/mast3r
+git submodule add https://github.com/agipro/pram.git imcui/third_party/pram
+git submodule add https://github.com/zju3dv/EfficientLoFTR.git imcui/third_party/EfficientLoFTR
+git submodule add https://github.com/OnderT/XoFTR.git imcui/third_party/XoFTR
