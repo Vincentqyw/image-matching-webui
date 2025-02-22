166edf59300c76d2272edb956db4115b1a310f44
Realcat <alpharealcat@gmail.com>
2025-02-22 17:32:57 +0000

    update: docker

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,1 +1,10 @@
-docker run -it -p 7860:7860 vincentqin/image-matching-webui:latest python app.py --server_name "0.0.0.0" --server_port=7860
+docker run -it -d \
+  -p 7860:7860 \
+  -p 8000:8000 \
+  -p 8001:8001 \
+  -p 8265:8265 \
+  --name imc-test \
+  vincentqin/image-matching-webui:latest
+
+# docker run -it --entrypoint /code/start.sh image-matching-webui:latest
+# docker run -it --entrypoint /bin/bash image-matching-webui:latest
